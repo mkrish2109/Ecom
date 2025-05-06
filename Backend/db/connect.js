8afd4eb8e440550cb52c618005ea5b3100b2360a
mkrish2109 <mkrish1106@gmail.com>
2025-05-06 17:27:02 +0530

    rename and remove

--- Backend/db/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = () => {
-  // console.log(process.env.MONGO_URI);
-  return mongoose.connect(process.env.MONGO_URI);
-};
-
-module.exports = connect;
